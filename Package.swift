78795b89c3f05598c675a600c25b66f7c4443f1e
Dmitry <sdkdimon@gmail.com>
2024-03-14 21:51:38 +0200

    Experimental FlowStacks branch

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/johnpatrickmorgan/FlowStacks", from: "0.4.0"),
+    .package(url: "https://github.com/sdkdimon/FlowStacks", .branchItem("sdkdimon/experimental-nav-stack")),
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.9.2"),
   ],
   targets: [
