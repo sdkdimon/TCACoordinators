5d01aa0f03b4228089b31e9f764e47ab86fc0bde
Dmitry <sdkdimon@gmail.com>
2024-03-15 13:12:01 +0200

    Used FlowStacks dependency without FlowNavigator
    - FlowNavigator is not used by TCACoordinators and cause a mem leak

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/sdkdimon/FlowStacks", .branchItem("sdkdimon/experimental-nav-stack")),
+    .package(url: "https://github.com/sdkdimon/FlowStacks", .branchItem("experimental-nav-stack-no-flow-navigator")),
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.9.2"),
   ],
   targets: [
